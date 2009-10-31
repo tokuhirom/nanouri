5d2833f9cc74cfe301f619b13ebe52680ef3640c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-31 11:34:09 +0900

    setup for release

--- lib/Clib/nanouri.pm
+++ lib/Clib/nanouri.pm
@@ -1,3 +1,33 @@
 package Clib::nanouri;
-our $VERSION = 0.01;
+our $VERSION = '0.01';
 1;
+__END__
+
+=head1 NAME
+
+Clib::nanouri - tiny URI parser for C/C++
+
+=head1 DESCRIPTION
+
+Tiny URI parser for C/C++.
+
+for more details, please see B<html/index.html>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno <tokuhirom@cpan.org>
+
+=head1 THANKS TO
+
+typester++ wrote AnyEvent::JSONRPC::Lite. This module takes A LOT OF CODE from that module =P
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2009 by tokuhirom.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
